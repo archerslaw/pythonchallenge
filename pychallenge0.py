c7e09bcfa2dd107e2dddd21cea9c7707181782c4
Sibiao Luo <sibiaoluo@gmail.com>
2013-08-12 17:01:03 +0800

    The first challenge asks for the value of 2^38.

---
+++ pychallenge0.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+
+"""
+Level 0: 0
+URL: http://www.pythonchallenge.com/pc/def/0.html
+The first challenge asks for the value of 2^38.
+"""
+
+def main():
+    #return pow(2, 38)
+    return 2**38
+
+if __name__ == '__main__':
+    print main()
+
